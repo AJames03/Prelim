16ff8478d13f637467ae5172574901c9726e9e9f
AJames03 <119918197+AJames03@users.noreply.github.com>
2022-12-06 14:17:59 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define p printf
+#define sc scanf
+
+int main()
+{
+    //Personal Information
+    p("------------------------------ REGISTRATION FORM ------------------------------- \n\n");
+    p("Name           : Betinol, Aron James L. \n");
+    p("Age            : 18 \n");
+    p("Birthday       : March 3, 2004 \n");
+    p("Address        : Blk 9 Lot 115 Phase 1A Sub-urban San Jose, Rodriguez, Rizal \n");
+    p("Contact Number : 09652635576 \n");
+    p("Email          : betinolaronjames529@gmail.com \n");
+    p("Citizenship    : Filipino \n");
+    p("Father's Name  : Paterno L. Betinol \n");
+    p("Mother's Name  : Cirila L. Betinol \n\n");
+
+    //For Education
+    p("----------------------------- EDUCATION ATTAINMENT ----------------------------- \n\n");
+    p("Primary        : Kasiglahan Village Elementary School \n");
+    p("Secondary      : Kasiglahan Village National High School \n");
+    p("Tertiary       : Asian Institute of Computer Studies \n\n");
+    //For College
+    p("1st Choice     : Bachelor of Science in Information Technology \n");
+    p("2nd Choice     : Bachelor of Science in Computer Science \n");
+    p("3rd Choice     : Bachelor of Science in Computer Engineering \n");
+
+}
