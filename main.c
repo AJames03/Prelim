2aeeac7bbdfe9f98d50a3ff8d59d43ae24fdc653
AJames03 <119918197+AJames03@users.noreply.github.com>
2022-12-06 14:19:26 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define p printf
+#define sc scanf
+
+// Betinol, Aron James L.
+// BSIT-1F
+// Find the Celsius and Fahrenheit
+int main()
+{
+    float f, ans;
+
+    p("Fahrenheit : ");
+    sc("%f", &f);
+    ans = (f-32)*5/9;
+
+    p("Celsius : %.2f \n", ans);
+
+    return 0;
+}
