15a4c5b0354e478a362fcc7e4f7768a9b73bc2fa
AJames03 <119918197+AJames03@users.noreply.github.com>
2022-12-06 14:22:31 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define p printf
+#define sc scanf
+
+// b is for length of the base
+// h is for the Height of the Triangle
+// area is for the area
+
+    float b, h, area;
+
+int main()
+{
+    p("\" Area of Triangle\" \n\n");
+
+    // For Base
+    p("Base = ");
+    sc("%f", &b);
+
+    //For Height
+    p("Height = ");
+    sc("%f", &h);
+
+    system("cls");
+
+    area = (b*h)/2;
+
+    // Answer/Equal/Total
+    p("Total Area = %.2f", area);
+
+    return 0;
+}
